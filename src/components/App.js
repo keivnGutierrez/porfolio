1e492016fff3ec26b2a061fbedb39861657b2dbd
Kevin Gutiérrez <kevinGutierrez6595@gmail.com>
2021-11-03 15:26:06 -0600

    initial config

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const App = () => {
+  return (
+    <main>
+      <header>
+        <h1>
+          Mi nombre es Kevin Gutiérrez Rodrígez, soy desarollador fontend.
+        </h1>
+      </header>
+    </main>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 9000,
+    open: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
